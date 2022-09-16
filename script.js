94eaebcd631548e9d2c7644ffb126468497dd68e
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-16 14:51:58 +0300

    2 files modified

---
+++ script.js
@@ -0,0 +1,54 @@
+// document.getElementById("test").innerHTML = "Hello JavaScript";
+
+// function myFunction() {
+//   document.getElementById("test").innerHTML = "Paragraph changed.";
+// }
+
+
+// let x, y, z;
+// x = 5;
+// y = 6;
+// z = x + y;
+// console.log(z)
+
+
+// let lastname, lastName;
+// lastName = "Doe";
+// lastname = "Peterson";
+
+
+// JS Variables: Var can declared again. Let can be declared once and its value can be changed. Conost value can never be changed
+
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+// document.write(z)
+
+
+// const price1 = 5;
+// const price2 = 6;
+// const total = price1 + price2;
+// total = 3 + 4;
+// document.write(total)
+
+
+// SyntaxError: 'x' has already been declared
+// let x = "John Doe";
+// let x = 0;
+
+// var x = "John Doe";
+// var x = 0;
+// document.write(x)
+
+
+//Variables declared with the var keyword can NOT have block scope.
+//Variables declared inside a { } block can be accessed from outside the block.
+
+
+// if (true){
+//   var x = 2;
+//   document.write(x)
+// }
+
+// x = 10;
+// document.write(x)
